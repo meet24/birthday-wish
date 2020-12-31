875a71f3e5d6301d90922e76b2e7100af8826b92
meet24 <39444181+meet24@users.noreply.github.com>
2020-12-31 11:47:08 +0530

    Add file via upload

---
+++ Birthday.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <dos.h>
+using namespace std;
+
+int main()
+{
+	char h = '\3';
+	Beep(523,500);
+	system("cls");
+	cout << endl << endl << endl << "\t\t\tFrom: \t COUSINS" << endl << "\t\t\tTo:\t DEV PARIKH" << endl << "\t\t";
+	Sleep(500);
+
+
+	for (int i = 0; i < 40; i++) {
+		cout << "_";
+	}
+	Sleep(500);
+	Beep(523, 500);
+
+	cout << endl << endl << "\t\t\t\t  i i i i i" << endl;
+	Sleep(500);
+	Beep(523, 500);
+	cout << "\t\t\t\t  i i i i i" << endl;
+	Sleep(500);
+	Beep(523, 500);
+	cout << "\t\t\t\t _i_i_i_i_i_";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t       |" << h << "\t    " << h << "|";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t     _|" << h << " 11/01/2020  " << h << "|_";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t    | " << h << "    " << h << "    " << h << "     " << h << " |";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t  _|" << h << "   HAPPY   " << h << "     " << h << "  |_";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t | " << h << "     " << h << "   BIRTHDAY    " << h << " |";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t |" << h << "  " << h << "  " << h << "  " << h << "  " << h << "  " << h << "  " << h << "  " << h << "  " << h << "|";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t |" << h << "  " << h << "  " << h << "  " << h << "  " << h << "  " << h << "  " << h << "  " << h << "  " << h << "|";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t | " << h << "  " << h << "      DEV      " << h << "  " << h << " |";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t |";
+	for (int i = 0; i < 25; i++) {
+		cout << "_";
+	}
+	cout << "|" << endl<<endl<<endl;
+
+	cout << "\t\tHAPPY BIRTHDAY";
+	cout << endl << "\t\t\t\tDEV";
+	cout << endl << "\t\t\t\t\tFROM";
+	cout << endl << "\t\t\t\t|||\t |||";
+	cout << endl << "\t\t\t\tMEET";
+	cout << endl << "\t\t\t\tDRASHTI";
+	cout << endl << "\t\t\t\tVAISHNAVI";
+	cout << endl << "\t\t\t\tKRISH";
+	cout << endl << "\t\t\t\tKUSHAL";
+	cout << endl << "\t\t\t\tABHI";
+	cout << endl << "\t\t\t\tSHIVAM";
+	cout << endl << "\t\t\t\tHIRAL " << h << " UMANG";
+	cout << endl << "\t\t\t\tKASHYAP " << h << " RIYA";
+	cout << endl << "\t\t\t\tPARTH " << h << " SHIVANI";
+	cout << endl << "\t\t\t\tKISHAN " << h << " HAPPY";
+}
